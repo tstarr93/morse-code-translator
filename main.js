95ef5f2ea9535b1ac289569b2f612f01878fde69
Tim Starr <timstarr14@gmail.com>
2021-03-30 14:48:01 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("#input-field");
+
+// Need to take what's written in the input-field
+
+
+// Run it through a function -> Takes each letter and determines its Morse equivalent
+
+// -> Returns each letter, translated, and put into a string
+
+// Take that string and put it in result-field using innerHTML
